a13b64437a19e93adaeeb972b5ff13b5251c9b65
Kiriti Komaragiri <kiriti.komaragiri.contractor@macmillan.com>
2023-06-17 02:13:58 +0530

    update terraform

---
+++ terraform/deployment/backend-config.tf
@@ -0,0 +1,5 @@
+bucket                      = "skillaro-terraform-states"
+key                         = "ci-cd-example.tfstate"
+region                      = "ap-south-1"
+encrypt                     = true
+dynamodb_table              = "terraform-locks
---
+++ terraform/deployment/vars.tf
@@ -0,0 +1,3 @@
+# General
+service_name		= "skillaro_service"
+aws_region          = "ap-south-1"
