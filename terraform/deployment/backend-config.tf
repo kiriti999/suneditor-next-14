672a13f2ce0fb88d8cb6662975c2c5526afe3f18
Kiriti Komaragiri <kiriti.komaragiri.contractor@macmillan.com>
2023-06-20 02:23:03 +0530

    add role

--- terraform/deployment/backend-config.tf
+++ terraform/deployment/backend-config.tf
@@ -1,6 +1,6 @@
 bucket = "skillaro-terraform-states"
 key = "ci-cd-example.tfstate"
-region = "skillaro-terraform-states"
+region = "ap-south-1"
 encrypt = true
 dynamodb_table = "terraform-locks"
 role_arn = "arn:aws:iam::516666139624:role/git_admin_role"
--- terraform/registry/config/backend-config.tf
+++ terraform/registry/config/backend-config.tf
@@ -3,3 +3,4 @@ key                         = "ci-cd-example.tfstate"
 region                      = "ap-south-1"
 encrypt                     = true
 dynamodb_table              = "terraform-locks"
+role_arn = "arn:aws:iam::516666139624:role/git_admin_role"
