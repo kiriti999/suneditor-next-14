4b905581c158d02702441fa64bf9a7fd0e8c14b8
Kiriti Komaragiri <kiriti.komaragiri.contractor@macmillan.com>
2023-06-20 10:44:07 +0530

    update tf

--- terraform/registry/config/backend-config.tf
+++ terraform/registry/config/backend-config.tf
@@ -1,6 +1,5 @@
 bucket                      = "skillaro-terraform-states"
-key                         = "ci-cd-example.tfstate"
+key                         = "terraform.tfstate"
 region                      = "ap-south-1"
 encrypt                     = true
 dynamodb_table              = "terraform-locks"
-role_arn = "arn:aws:iam::516666139624:role/git_admin_role"
