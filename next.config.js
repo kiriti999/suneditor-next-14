91f77637f8f9a41245162626f6c08921c69c93bf
kiriti <support@rootscope.in>
2023-06-21 21:40:15 +0530

    add env vars

--- next.config.js
+++ next.config.js
@@ -11,6 +11,13 @@ const nextConfig = nextTranslate({
   },
   env: {
     JWT_SECRET: "djhfghbdsgrasklkajsdgf",
+    CLOUDINARY_UPLOAD_PRESET: "skillaro",
+    CLOUDINARY_NAME: "cloudinary999",
+    CLOUDINARY_API_KEY: "429854764125427",
+    CLOUDINARY_SECRET: "PxEdUKCLC9Shs-DJbMWUEmZGC-s",
+    CLOUDINARY_VIDEO_URL: "https://api.cloudinary.com/v1_1/cloudinary999/video/upload",
+    CLOUDINARY_IMAGE_UPLOAD_URL: "https://api.cloudinary.com/v1_1/cloudinary999/image/upload",
+    ALGOLIA_ADMIN_KEY: "183f7ddb740690df8b6fe7cd82008198"
   }
 })
 
