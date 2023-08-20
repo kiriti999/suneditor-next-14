d174749120522e1bb6d5fecd57d13b617d8ce14e
Kiriti Komaragiri <kiriti.komaragiri.contractor@macmillan.com>
2023-08-20 05:39:17 +0530

    show become a teacher tab only if user is not a teacher and not a admin

--- components/_App/Navbar.js
+++ components/_App/Navbar.js
@@ -115,7 +115,7 @@ const Navbar = ({ user }) => {
 
 									</li>
 
-									{user && !isTeacher && (
+									{(user && !isTeacher && !isAdmin) && (
 										<li className="nav-item">
 											<Link href="/become-a-teacher">
 												<a
