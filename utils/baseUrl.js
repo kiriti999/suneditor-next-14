04620957460409b7be32511f13c0846094e21b28
Kiriti Komaragiri <kiriti.komaragiri.contractor@macmillan.com>
2023-10-02 13:50:57 +0530

    update port

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,4 +1,4 @@
 export const axiosApi = {
-    baseUrl: process.env.NODE_ENV === "production" ? 'https://api.whatsnxt.in' : `http://localhost:3001`,
+    baseUrl: process.env.NODE_ENV === "production" ? 'https://api.whatsnxt.in' : `http://localhost:4444`,
     blogApiBaseUrl: process.env.NODE_ENV === "production" ? 'https://blogapi.whatsnxt.in/api/blog' : `http://localhost:3333/api/blog`
 }
