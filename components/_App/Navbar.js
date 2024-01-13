3f32a5e9a6fad45ab8a37398ee8ad28dffa48954
Kiriti <kiriti.k999@gmail.com>
2024-01-13 11:17:21 +0530

    mobile view enhancements

--- components/_App/Navbar.js
+++ components/_App/Navbar.js
@@ -11,12 +11,17 @@ const Navbar = ({ user }) => {
 	const { userObject } = useSelector((state) => state.user);
 	const dispatch = useDispatch();
 	const [menu, setMenu] = useState(true);
+	const [displaySearch, setDisplaySearch] = useState(false);
 
 	user = userObject !== null ? userObject : user;
 
 	const toggleNavbar = () => {
 		setMenu(!menu);
 	};
+
+	const toggleSearch = () => {
+		setDisplaySearch(!displaySearch);
+	}
 
 	useEffect(() => {
 		let elementId = document.getElementById("navbar");
@@ -41,34 +46,155 @@ const Navbar = ({ user }) => {
 			<div id="navbar" className={styles['navbar-area']}>
 				<div className={styles['whatsnxt-nav']}>
 					<div className={`container-fluid ${styles['container-fluid']}`}>
-						<div className={`navbar navbar-expand-lg navbar-light ${styles['navbar']}`}>
+						<div className={`navbar navbar-expand-lg navbar-light ${styles['navbar']} ${displaySearch ? 'd-none' : ''}`}>
 							<Link legacyBehavior href="/">
 								<a
 									onClick={toggleNavbar}
 									className={`navbar-brand ${styles['navbar-brand']}`}
 								>
-									<img src="/images/logo.png" alt="logo" style={{ width: '200px', height: '100px' }} />
+									<img src="/images/logo.png" alt="logo" />
 								</a>
 							</Link>
 
-							<button
-								onClick={toggleNavbar}
-								className={classTwo}
-								type="button"
-								data-toggle="collapse"
-								aria-expanded="false"
-								aria-label="Toggle navigation"
-							>
-								<span className={`${styles['icon-bar']} ${styles['top-bar']}`}></span>
-								<span className={`${styles['icon-bar']} ${styles['middle-bar']}`}></span>
-								<span className={`${styles['icon-bar']} ${styles['bottom-bar']}`}></span>
-							</button>
+							<div className="d-flex d-lg-none align-items-center" onClick={toggleNavbar}>
+								<span className="me-2">Menu</span>
+								<i className={`bx ${menu ? 'bxs-up-arrow' : 'bxs-down-arrow'}`}></i>
+							</div>
+
+							<div className="d-flex d-lg-none align-items-center" onClick={toggleSearch}>
+								<i className="bx bx-search"></i>
+							</div>
+
+							<div className={`${styles['others-option']} d-flex align-items-center order-lg-1`}>
+								<div className={styles['option-item']}>
+									<div className={styles['cart-btn']}>
+										<Link legacyBehavior href="/cart">
+											<a>
+				 								<i className="flaticon-shopping-cart"></i>{" "}
+												<span>
+													{cartItems.length}
+												</span>
+											</a>
+										</Link>
+									</div>
+								</div>
+
+								<div className={styles['option-item']}>
+									{user ? (
+										<div className={styles['user-dropdown']}>
+											<Link legacyBehavior href="/">
+												<a
+													onClick={(e) => e.preventDefault()}
+													className={`default-btn ${styles['default-btn']}`}>
+													<i className="flaticon-user"></i>{" "}
+													{user.name}{" "}
+													<span></span>
+												</a>
+											</Link>
+
+											<ul className={`dropdown-menu ${styles['dropdown-menu']}`}>
+												<li className={`nav-item ${styles['nav-item']}`}>
+													<Link legacyBehavior
+														href="/my-courses"
+														activeClassName={`active ${styles['active']}`}>
+														<a onClick={toggleNavbar}
+															className="nav-link">
+															My Courses
+														</a>
+													</Link>
+												</li>
+
+												<li className={`nav-item ${styles['nav-item']}`}>
+													<Link legacyBehavior
+														href="/user/my-profile"
+														activeClassName={`active ${styles['active']}`}>
+														<a
+															onClick={toggleNavbar}
+															className="nav-link">
+															My Profile
+														</a>
+													</Link>
+												</li>
+
+												<li className={`nav-item ${styles['nav-item']}`}>
+													<Link legacyBehavior
+														href="/user/edit-profile"
+														activeClassName={`active ${styles['active']}`}>
+														<a
+															onClick={toggleNavbar}
+															className="nav-link">
+															Edit Profile
+														</a>
+													</Link>
+												</li>
+
+												<li className={`nav-item ${styles['nav-item']}`}>
+													<Link legacyBehavior
+														href="/user/edit-password"
+														activeClassName={`active ${styles['active']}`}>
+														<a
+															onClick={toggleNavbar}
+															className="nav-link">
+															Edit Password
+														</a>
+													</Link>
+												</li>
+
+												<li className={`nav-item ${styles['nav-item']}`}>
+													<Link legacyBehavior href="/">
+														<a
+															className="nav-link"
+															onClick={(e) => {
+																e.preventDefault();
+																dispatch({
+																	type: 'UPDATE_USEROBJ',
+																	data: null
+																});
+																dispatch({
+																	type: 'UPDATE_CART',
+																	data: { cartItems: [], discount: 0 }
+																});
+																localStorage.removeItem('cart');
+																signOut();
+																handleLogout();
+															}}>
+															Logout
+														</a>
+													</Link>
+												</li>
+											</ul>
+										</div>
+									) : (
+										<Link legacyBehavior href="/authentication">
+											<a className={`default-btn ${styles['default-btn']}`}>
+												<i className="flaticon-user"></i>{" "}
+												<div className="d-none d-lg-inline">Login/Register</div>
+												<div className="d-inline-block d-lg-none">Login</div>
+												<span></span>
+											</a>
+										</Link>
+									)}
+								</div>
+
+
+								{/* <div className={styles['option-item']}>
+									<Link legacyBehavior href="/contact">
+										<a className="default-btn">
+											<i className="flaticon-user"></i>{" "}
+											Contact us <span></span>
+										</a>
+									</Link>
+								</div> */}
+
+							</div>
 
 							<div
 								className={classOne}
 								id="navbarSupportedContent"
 							>
-								<SearchForm />
+								<div className="d-none d-md-block">
+									<SearchForm />
+								</div>
 
 								<ul className={`navbar-nav ${styles['navbar-nav']}`}>
 									<li className={`nav-item ${styles['nav-item']}`}>
@@ -154,128 +280,12 @@ const Navbar = ({ user }) => {
 										</li>
 									)}
 								</ul>
-
-								<div className={`${styles['others-option']} d-flex align-items-center`}>
-									<div className={styles['option-item']}>
-										<div className={styles['cart-btn']}>
-											<Link legacyBehavior href="/cart">
-												<a>
-													<i className="flaticon-shopping-cart"></i>{" "}
-													<span>
-														{cartItems.length}
-													</span>
-												</a>
-											</Link>
-										</div>
-									</div>
-
-									<div className={styles['option-item']}>
-										{user ? (
-											<div className={styles['user-dropdown']}>
-												<Link legacyBehavior href="/">
-													<a
-														onClick={(e) => e.preventDefault()}
-														className={`default-btn ${styles['default-btn']}`}>
-														<i className="flaticon-user"></i>{" "}
-														{user.name}{" "}
-														<span></span>
-													</a>
-												</Link>
-
-												<ul className={`dropdown-menu ${styles['dropdown-menu']}`}>
-													<li className={`nav-item ${styles['nav-item']}`}>
-														<Link legacyBehavior
-															href="/my-courses"
-															activeClassName={`active ${styles['active']}`}>
-															<a onClick={toggleNavbar}
-																className="nav-link">
-																My Courses
-															</a>
-														</Link>
-													</li>
-
-													<li className={`nav-item ${styles['nav-item']}`}>
-														<Link legacyBehavior
-															href="/user/my-profile"
-															activeClassName={`active ${styles['active']}`}>
-															<a
-																onClick={toggleNavbar}
-																className="nav-link">
-																My Profile
-															</a>
-														</Link>
-													</li>
-
-													<li className={`nav-item ${styles['nav-item']}`}>
-														<Link legacyBehavior
-															href="/user/edit-profile"
-															activeClassName={`active ${styles['active']}`}>
-															<a
-																onClick={toggleNavbar}
-																className="nav-link">
-																Edit Profile
-															</a>
-														</Link>
-													</li>
-
-													<li className={`nav-item ${styles['nav-item']}`}>
-														<Link legacyBehavior
-															href="/user/edit-password"
-															activeClassName={`active ${styles['active']}`}>
-															<a
-																onClick={toggleNavbar}
-																className="nav-link">
-																Edit Password
-															</a>
-														</Link>
-													</li>
-
-													<li className={`nav-item ${styles['nav-item']}`}>
-														<Link legacyBehavior href="/">
-															<a
-																className="nav-link"
-																onClick={(e) => {
-																	e.preventDefault();
-																	dispatch({
-																		type: 'UPDATE_USEROBJ',
-																		data: null
-																	});
-																	dispatch({
-																		type: 'UPDATE_CART',
-																		data: { cartItems: [], discount: 0 }
-																	});
-																	localStorage.removeItem('cart');
-																	signOut();
-																	handleLogout();
-																}}>
-																Logout
-															</a>
-														</Link>
-													</li>
-												</ul>
-											</div>
-										) : (
-											<Link legacyBehavior href="/authentication">
-												<a className={`default-btn ${styles['default-btn']}`}>
-													<i className="flaticon-user"></i>{" "}
-													Login/Register <span></span>
-												</a>
-											</Link>
-										)}
-									</div>
-
-
-									{/* <div className={styles['option-item']}>
-										<Link legacyBehavior href="/contact">
-											<a className="default-btn">
-												<i className="flaticon-user"></i>{" "}
-												Contact us <span></span>
-											</a>
-										</Link>
-									</div> */}
-
-								</div>
 							</div>
+						</div>
+
+						<div className={`align-items-center d-lg-none ${displaySearch ? 'd-flex' : 'd-none'}`}>
+							<SearchForm />
+							<h2 className="ms-2 bx bx-x" onClick={toggleSearch}></h2>
 						</div>
 					</div>
 				</div>
